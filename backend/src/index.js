c1da0ea4cd91dfd40c64f5e9518923e3932a11b5
anushkastar <162957357+anushkastar@users.noreply.github.com>
2024-08-26 01:49:23 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import "dotenv/config"
 import mongoose from "mongoose"
+import userRoutes from "./Routes/UserRoute.js";
 
 mongoose.connect(process.env.MONGODB)
 
@@ -10,10 +11,12 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
 
+app.use("/api/v1/users", userRoutes);
+
 app.get("/api/test",async(req,res)=>{
     res.json({message:"Hello"})
 });
 
-app.listen(7000,()=>{
+app.listen(8000,()=>{
     console.log("server running")
 })

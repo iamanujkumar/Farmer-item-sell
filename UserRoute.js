3690bda723d47adbb48938b4bb28f61b90e30d2b
anushkastar <162957357+anushkastar@users.noreply.github.com>
2024-08-26 01:46:42 +0530

    Delete UserRoute.js

--- UserRoute.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {login, register} from "../Controller/UserConroller.js"
-
-const router= Router();
-
-router.route("/login").post(login)
-router.route("/register").post(register)
-
-export default router;

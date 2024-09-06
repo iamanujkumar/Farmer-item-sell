4d8de8e195938ca5752194b93aa736c7709b4a40
Amit Kumar Sahani <121219168+amit-23-hub@users.noreply.github.com>
2024-09-06 22:28:45 +0530

    business component , dash board , discountStripe

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,14 +1,10 @@
 import Chatbot from "../components/ChatBot/Chatbot"
 import Category from "../components/Category/Category"
-<<<<<<< HEAD
-import Discount from "../components/Discount/Discount"
-import BusinessComponent from "../components/BussinessComponent/BussinessComponent"
-import RecommendationProducts from "../components/Recomendation/recommendedProduct"
-=======
 import HeroSection from "./Hero/HeroSection"
 import SummarySection from "../components/Summery/SummarySection"
 import BusinessComponent from "../components/BusinessComponent/BusinessComponent"
->>>>>>> b81168c43915d4fb98e7342d81d72ebceca88e94
+import RecommendationProducts from "../components/Recomendation/recommendedProduct"
+import Discount from "../components/Discount/Discount"
 
 const Home = () => {
   return (
@@ -17,15 +13,10 @@ const Home = () => {
       {/* <img src={cover} alt="" /> */}
       <HeroSection/>
       <Category/>
-<<<<<<< HEAD
-      <Discount/>
-      <BusinessComponent/>
       <RecommendationProducts/>
-      hii
-=======
       <SummarySection/>
-     
->>>>>>> b81168c43915d4fb98e7342d81d72ebceca88e94
+     <BusinessComponent/>
+     <Discount/>
     </div>
   )
 }
